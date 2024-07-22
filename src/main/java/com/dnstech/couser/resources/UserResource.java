89165bf4c3926f0797b4e89db1ec6eb4b7baadc3
Douglas nunes <donsilva-dev>
2024-07-22 13:49:33 -0300

    User entity and resource

---
+++ src/main/java/com/dnstech/couser/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.dnstech.couser.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dnstech.couser.entities.User;
+
+/* - Aqui iremos definir os recursos basico para aplicação "User" que serão os controladores rest 
+ * - Para esse classe ser um recurso web é usado a anotação @RestController
+ * - Alem disso damos um nome ao recurso usando anotação @RestMapping */
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	/*Criando um metodo endpoint para acessar os usuarios 
+	 * ResponseEntitis é o tipo expecifico de retorno do sring para retorna resposta de requisiçoes web*/
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Douglas", "douglas@teste.com", "11947982565", "123456789");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+
+}
